a91b0258c9c757cedaa296d1109c0c09f7032b08
aliya-sabbah <aliyasabbah266@gmail.com>
2025-05-03 15:35:04 +0300

    index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <title>Final Project</title>
+</head>
+<body>
+    <!--Section with class Number one-->
+    <section class="one">
+        <div class="container">
+        <i class="fa-regular fa-user" style="color:rgb(200,65,10);"></i>
+        <p>Welcome to Music Festival 2023</p>
+    </div>
+    </section>
+    <!--==================================-->
+<!--Navbar-->
+<nav>
+  <div class="container">
+    <div class="aa">
+    <h2 style="color: white;">Festava Live</h2> 
+    <ul>
+      <li><a href="#" style="color: rgb(200,65,10);">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Artists</a></li>
+      <li><a href="#">Schedule</a></li>
+      <li><a href="#">Picing</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><button type="button" >Buy Ticket</button></li>
+    </ul>
+  </div>
+   <div class="bb">
+    <div class="part1">
+      <p>FESTAVALIVEPRESENTS</p>
+      <h1>Night Live 2023</h1>
+    </div>
+    <br></br>
+     <div class="part2"><button type="button">Lets begin</button></div>
+   </div>
+  <div class="cc">
+    <div class="part3" style="margin-left: 0px;">
+      <i class="fa-regular fa-clock"></i>
+      <h2>10-12 ,Dec2023</h2>
+    </div>
+    <div class="part4">
+      <i class="fa-solid fa-location-dot"></i>
+      <h2>Lorem ipsum dolor sit.</h2>
+    </div>
+    <div class="part5">
+      <p>Share:</p>
+      <div class="icon"><i class="fa-brands fa-facebook"></i></div>
+      <div class="icon"><i class="fa-brands fa-twitter"></i></div>
+       <div class="icon"><i class="fa-brands fa-instagram"></i></div>
+    </div>
+  </div>
+</nav>
+<!--================================-->
+<section class="web">
+  <div class="container">
+    <div class="dd">
+   <h1>About Festava 2022</h1>
+   <br></br>
+   <p>Festava Live is free CSS template provided by TemplateMo website.This layout</p>
+   <p> is bulit on Bootstrape v5.2.2 CSS library.You are free to use this template for your</p>
+   <p>commerical website.</p>
+    <br></br>
+    <h3>Once in Lifetime Experience</h3>
+    <br></br>
+   <p>You are not allowed to redistribute the template ZIP file on any other website</p> 
+   <p>without a permission</p> 
+   <br></br>
+   <h3>Whole Night Party</h3>
+   <br></br>
+   <p>Please tell your friends about our website. Thank you.</p>
+  </div>
+    <div class="all">
+   <img src="./images/pexels-alexander-suhorucov-6457579.jpg" alt="no photo" width="450px" height="300px">
+   <div class="aliya">
+    <i class="fa-regular fa-user" style="color:rgb(200,65,10);"></i>
+    <h1 style="color: black;">a happy moment</h1>
+   <p>your amazing Festival experience with us</p>
+  </div>
+  </div>
+</section>
+  <!--=================================-->
+  <section class="ee">
+    <div class="container">
+      <div class="part6"><h1>Meet Artists</h1></div>
+      <div class="part7">
+        <div class="image-one"></div>
+      <div class="part8">
+        <div class="image-tow"></div>
+        <div class="image-three"></div> 
+      </div>
+    </div>
+  </section>
+  <!--=================================-->
+  <section class="ff">
+    <div class="container">
+      <div class="part9"><h1>Event Schedule</h1></div>
+      <div class="Table">
+        <table border="1">
+          <thead>
+          <tr style="height: 10vh;">
+            <th>Date</th>
+            <th>wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><div><p style="padding-top: 50px;">Day 1</p></div></td>
+            <td style="background-image: url(./images/artists/joecalih-UmTZqmMvQcw-unsplash.jpg);width: 450px;height: 250px;">
+              <div class="part10">
+              <h1>Pop nighht</h1>
+              <p style="margin-top: 10px;">5:00 - 7:00 PM</p>
+              <P style="margin-top: 10px;"> By Adele</P>
+            </div></td>
+            <td  style="background-color: rgb(241,221,214);"><div></div></td>
+            <td style="background-image:url(./images/artists/abstral-official-bdlMO9z5yco-unsplash.jpg);width:380px;height: 200px;">
+              <div class="part11">
+              <h1>Rock &Roll</h1>
+              <p style="margin-top: 10px;">7:00 - 11:00 PM</p>
+              <P style="margin-top: 10px;"> By Rihana</P>
+            </div></td>
+          </tr>
+          <tr>
+            <td><div><p>Day 2</p></div></td>
+            <td  style="background-color: rgb(236,200,200);"><div></div>
+            </td>
+           <td>
+            <div class="part11">
+              <h1>DJ Night</h1>
+              <p style="margin-top: 10px;">6:30 - 9:30 PM</p>
+              <P style="margin-top: 10px;"> By Rihana</P>
+            </div></td>
+            <td  style="background-color: rgb(216,226,217);"><div></div></td>
+          </tr>
+          <tr>
+            <td><div><p>Day 3</p></div></td>
+            <td style="background-image:url(./images/artists/soundtrap-rAT6FJ6wltE-unsplash.jpg);width: 450px;height: 200px">
+              <div class="part11">
+              <h1>Country Music</h1>
+              <p style="margin-top: 10px;">4:30 - 7:30 PM</p>
+              <P style="margin-top: 10px;"> By Rihana</P>
+            </div></td>
+            <td  style="background-color: rgb(207,208,194);"><div></div></td>
+            <td><div>
+              <h1>Free Style</h1>
+              <p style="margin-top: 10px;">6:00 - 10:00 PM</p>
+              <P style="margin-top: 10px;"> By Members</P>
+            </div></td>
+          </tr>
+        </tbody>
+        </table>
+  </div>
+  </div>
+</section>
+         <!--=================================-->
+          <section class="gg">
+          <div class="container">
+           <div class="part13"><h1>Plans, You love</h1></div>
+            <div class="section">
+            <div class="section1">
+              <div class="all">
+              <div><p><b>Early Bird $120</b></p>
+              <p style="margin-top: 10px;color: rgb(122,125,130);"> Including good things</p></div>
+              <div class="circle" style="text-align: center;">
+                <p style="color: white;">Save to up</p>
+                <h2 style="color: white;">50%</h2>
+              </div>
+            </div>
+      
+              <div class="towpart">
+              <div class="part14" style="list-style-type: circle;padding-left: 20px;">
+                <ul>
+                <li><p>paltform for potential <br>customers</p></li>
+                <li><p style="margin-top: 10px;">digital experience</p></li>
+              </ul>
+              </div>
+              <div class="part15" style="list-style-type: circle;padding-right: 0px;padding-left:60px ;">
+                <ul>
+                <li><p>high-quality sound</p></li>
+                <li><p style="margin-top: 10px;">standard content</p></li>
+              </ul>
+              </div>
+            </div>
+            <br></br>
+            <a href="#" style="color:rgb(245,208,67);">By Ticket</a>
+            <i class="fa-solid fa-angle-right" style="color:rgb(245,208,67);"></i> 
+            </div>
+             <div class="section2">
+             <p><b>Early Bird$240</b></p>
+             <p style="margin-top: 10px;color: rgb(122,125,130);">what makes a premium festava?</p>          
+             <div class="towpart">
+             <div class="part16" style="padding-left: 20px;">
+              <ul>
+              <li><p>paltform for potential <br> customers</p></li>
+              <li><p style="margin-top: 10px;">digital experience</p></li>
+              </ul>
+             </div>
+             <div class="part17" style="padding-left:80px ;">
+              <ul>
+               <li><p>high-quality sound</p></li>
+               <li><p style="margin-top: 10px;">premium content</p></li>
+               <li><p style="margin-top: 10px;">live chat support</p></li>
+              </ul>
+             </div>
+            </div>
+            <br></br>
+            <a href="#" style="color:rgb(245,208,67);">By Ticket</a>
+            <i class="fa-solid fa-angle-right" style="color:rgb(245,208,67);padding: 15px;"></i>
+           </div>
+          </div>
+        </div>
+        </section>
+        <!--=================================================-->
+          <section>
+          <div class="container">
+            <div class="part18"><h1>Intersted? Lets talk</h1>
+            <br>
+            <div class="twobutton" style=" background-color: rgb(239,248,255);  border-radius: 30px; padding: 15px; width: 30%;align-items: center;">
+              <button type="button" style="background-color: white;color: rgb(246,202,33);font-weight: 80px;">Content Form</button>
+              <button type="button" style="border: 0;background-color:rgb(239,248,255);color: rgb(108,113,119);">Google Maps</button>
+            </div>
+          </div>
+            <br></br>
+            <form action="" class="FORM">
+            <div class="Input">
+              <input type="text" placeholder="Full name" style="padding:10px 35px;margin-top: 30px;margin-bottom: 70px;border-radius: 5px;">
+              <input type="text" placeholder="Email addresss"style="padding:10px 35px;margin-top: 10px;border-radius: 5px;">
+              <br></br>
+              <input type="text" placeholder="Company"style="padding:10px 170px;margin-top:0px;border-radius: 5px;">
+              <br></br>
+              <textarea  cols="70" rows="2" placeholder="Message"style="padding:15px 10px;margin-top: 10px;margin-bottom: 70px;border-radius: 5px;"></textarea>
+              <br></br>
+              <button type="button" style="padding: 10px 40px; margin-bottom: 20px;">Send Message</button>
+            </div>
+          </form>
+          </div>
+        </section>
+        <!--==================================-->
+        <section class="ii">
+          <div class="container">
+            <div class="title">
+          <h1>Festava Live</h1>
+        </div>
+        <div class="all_icon">
+          <div class="icon"><i class="fa-brands fa-twitter"></i></div>
+            <div class="icon"><i class="fa-brands fa-apple"></i></div>
+            <div class="icon"><i class="fa-brands fa-instagram"></i></div>
+            <div class="icon"><i class="fa-brands fa-youtube"></i></div>
+            <div class="icon"><i class="fa-brands fa-pinterest"></i></div>
+        </div>
+      </div>
+      </section>
+     <!--======================================-->
+    <footer>
+      <div class="container">
+        <div class="link">
+          <div class="part21">
+            <h2 style="color: rgb(255, 207,45);">Links</h2>
+            <ul style="padding-right: 16px;">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Artists</a></li>
+              <li><a href="#">Schedule</a></li>
+              <li><a href="#">Picing</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <div class="part22">
+            <h2 style="color: rgb(255, 207,45);">Have a question?</h2> 
+            <p>090-080-0760</p>
+            <p>hello@company.com</p>
+          </div>
+
+          <div class="part23">
+            <h2 style="color: rgb(255, 207,45);">Location</h2> 
+            <p>Silang Junction South, Tagaytay,</p>
+            <p> Cavite, Philippines</p> 
+            <br></br>
+            <a href="#" style="color:white;">Our Maps</a>
+            <i class="fa-solid fa-angle-right" style="color: white;padding: 15px;"></i>
+          </div>
+        </div> 
+      </div>
+     </footer>
+     <!--===============================-->
+     <footer class="end">
+      <div class="container">
+        <ul>
+          <li>Copyright2036 Festava Live Company</li>
+          <li>Terms&Conditions</li>
+          <li>Privacy Policy</li>
+          <li>Your Feedback</li>
+        </ul>
+
+      </div>
+
+     </footer>
+ 
+</body>
+</html>
+ 
